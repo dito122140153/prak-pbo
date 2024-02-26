da2641aa2fcd0b71b292fede2251ef96aa4129be
dito122140153 <161263968+dito122140153@users.noreply.github.com>
2024-02-26 08:17:32 +0700

    Update 122140153_Dito Rifki Irawan_Prak1_soal1.py

--- prak1/122140153_Dito Rifki Irawan_Prak1_soal1.py
+++ prak1/122140153_Dito Rifki Irawan_Prak1_soal1.py
@@ -1,1 +1,16 @@
+def penjumlahanganjil(bawah,atas):
+  if bawah < 0 or atas < 0 :
+    print("Batas bawah dan atas yang dimasukan tidak boleh dibawah Nol")
 
+  jumlahganjil = 0
+  for i in range (bawah, atas):
+    if i % 2 != 0:
+      print(i)
+      jumlahganjil+=1
+      return jumlahganjil
+
+bawah = int(input("batas bawah : "))
+atas = int(input("batas atas : "))
+
+jumlahganjil = penjumlahanganjil(bawah, atas)
+print("Total : ",jumlahganjil)
